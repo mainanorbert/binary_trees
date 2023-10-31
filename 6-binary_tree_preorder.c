e321cf2569de83c081e36f5c99be9416bac05276
mainanorbert <mainanorbert@gmail.com>
2023-10-31 06:43:09 +0000

    prorder traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -10,7 +10,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 
 	if (tree == NULL || func == NULL)
 		return;
-	binary_tree_preorder(tree->left, f);
 	f(tree->n);
+	binary_tree_preorder(tree->left, f);
 	binary_tree_preorder(tree->right, f);
 }
