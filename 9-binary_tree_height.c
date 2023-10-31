82cc6bb822d80b5c0e9f4ed7180729238a458707
mainanorbert <mainanorbert@gmail.com>
2023-10-31 07:49:41 +0000

    height of tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,13 +10,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
+	if (left_h >= right_h)
+		return (left_h + 1);
 	else
-	{
-		left_h= binary_tree_height(tree->left);
-		right_h = binary_tree_height(tree->right);
-		if (left_h >= right_h)
-			return (left_h + 1);
-		else
-			return (right_h + 1);
-	}
+		return (right_h + 1);
 }
